af8fdc704de591c8ca4e70a557d251281b86713a
Marco Kaul <marco.kaul@helpling.com>
2017-09-15 15:05:20 +0200

    Add test for 1

--- stringcalculator.js
+++ stringcalculator.js
@@ -1,4 +1,5 @@
 function add(numbers) {
+  if (numbers === '1') return 1;
   return 0;
 }
 
--- stringcalculator.test.js
+++ stringcalculator.test.js
@@ -1,5 +1,9 @@
 const { add } = require('./stringcalculator');
 
-test('return 0 on empty string', () => {
+test('returns 0 on empty string', () => {
   expect(add('')).toBe(0);
+});
+
+test('returns 1 on passing 1', () => {
+  expect(add('1')).toBe(1);
 });
