bb873b4e5221695f4f96bade560e25f55a7d4ca0
Marco Kaul <marco.kaul@helpling.com>
2017-09-15 15:08:10 +0200

    Add test 1,2 == 3

--- stringcalculator.js
+++ stringcalculator.js
@@ -1,5 +1,6 @@
 function add(numbers) {
   if (numbers === '1') return 1;
+  if (numbers === '1,2') return 3;
   return 0;
 }
 
--- stringcalculator.test.js
+++ stringcalculator.test.js
@@ -7,3 +7,7 @@ test('returns 0 on empty string', () => {
 test('returns 1 on passing 1', () => {
   expect(add('1')).toBe(1);
 });
+
+test('returns 3 on passing 1,2', () => {
+  expect(add('1,2')).toBe(3);
+});
